d5cad9d021c26f189b993e8ebaa101e4c7ff4783
Carlos Triana <ctriana@tiresia.co>
2025-05-09 13:22:29 -0500

    entities

--- Backend/src/app.module.ts
+++ Backend/src/app.module.ts
@@ -2,10 +2,26 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductosModule } from './productos/productos.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
+import { ProductoModule } from './producto/producto.module';
 
 @Module({
-  imports: [ProductosModule],
+  imports: [
+    ProductosModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql', // o 'postgres', 'sqlite', etc.
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '',
+      database: 'pruebas',
+      autoLoadEntities: true, // Carga autom�ticamente los Entity registrados en cualquier m�dulo
+    }),
+    UserModule,
+    ProductoModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
